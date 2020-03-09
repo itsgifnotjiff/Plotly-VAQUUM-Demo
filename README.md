ccb0a4b1d151ecb7068c3739c1a9bdd8325f8216
Yordan Radev <yordan.radev@canada.ca>
2020-03-09 12:55:49 -0400

    GeoMet Added

--- README.md
+++ README.md
@@ -2,9 +2,18 @@ CMC-Dorval Plotly Demo for VAQUUM and my sweatest darling Si Jun ;)
 
 TO-DO
 
+
+- [DONE] X-Axis Date format 
+- [DONE] X-Axis rotate 
+- [DONE] Add color line grouping 
+- [DONE] Add different symbols
 - Add LaTeX Axis Title support
-- Add up to 10 models support 
+- [DONE] Add up to 10 models support 
     Ask Mr, Sylvain and Mr. Rodrigo about styling
-- Add export CSV functionality and PNG instead of SVG
-- Make the the layout responsive
+- Add export CSV functionality 
+- [DONE] Add PNG export instead of SVG
+- [DONE] Make the the layout responsive
 - Add a Dark Mode button
+- [DONE] Add map
+- [DONE] Add JSON parser
+- [DONE] Remove some controls (Logo and edit)
--- plotly-tester.js
+++ plotly-tester.js
@@ -199,6 +199,8 @@ Plotly.d3.json(url, (err, raw) =>
 }
 */
 
+/*
+
 var data_map = 
 [
 	{
@@ -219,7 +221,8 @@ var layout_map =
   autosize: true,
   hovermode: 'closest',
   mapbox: { style: "dark", bearing: 0 , center: { lat: 45 , lon: -73 } , pitch: 0 , zoom: 5 },
-  margin: { t: 25 , b: 0 , l: 0 , r: 0 }
+  margin: { t: 25 , b: 0 , l: 0 , r: 0 },
+
 }
 
 const config_map = 
@@ -243,10 +246,51 @@ const config_map =
 
 Plotly.newPlot( 'map' , data_map , layout_map , config_map )
 
+*/
 
 // Boum !!!! JSON, csv, tsv and xml parser
 
-var d3_parser = Plotly.d3.json("ressources/exampleData.json" , function (data) { console.log(data); });
+// var d3_parser = Plotly.d3.json("ressources/exampleData.json" , function (data) { console.log(data); });
 
 
 
+Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2015_06_30_precipitation.csv', function(err, rows){
+      function unpack(rows, key) {
+          return rows.map(function(row) { return row[key]; });
+		}
+
+var data = [{
+        type: 'scattermapbox', text: unpack(rows, 'Globvalue'),
+        lon: unpack(rows, 'Lon'), lat: unpack(rows, 'Lat'),
+        marker: {color: 'fuchsia', size: 4}
+    }];
+
+var layout = {
+	dragmode: 'zoom',
+	mapbox: {
+		style: 'white-bg',
+		layers: 
+		[
+			{
+            "below": 'traces',
+            "sourcetype": "raster",
+            "source": 
+			[
+				"https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+			]
+        	},
+			{
+				sourcetype: "raster",
+				source: 
+				[
+					"https://geo.weather.gc.ca/geomet/?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX={bbox-epsg-3857}&CRS=EPSG:3857&WIDTH=1000&HEIGHT=1000&LAYERS=RADAR_1KM_RDBR&TILED=true&FORMAT=image/png"
+				]
+			}
+		],
+		below: 'traces',
+		center: {lat: 38, lon: -90}, zoom: 4},
+	margin: {r: 0, t: 0, b: 0, l: 0},
+	showlegend: false};
+
+Plotly.newPlot('map', data, layout);
+  });
